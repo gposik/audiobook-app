a669931e7fcadc9c30e7311bddb9ac24f7312d5c
Gastón <gposik@gmail.com>
2022-05-16 18:35:32 -0300

    Created .env and .env.example files and some configuration improvements

--- libs/mailgun.py
+++ libs/mailgun.py
@@ -1,13 +1,13 @@
+import os
 from typing import List
 from requests import Response, post
 
 
 class Mailgun:
-
-    MAILGUN_DOMAIN = ""
-    MAILGUN_API_KEY = ""
+    MAILGUN_DOMAIN = os.environ.get("MAILGUN_DOMAIN")
+    MAILGUB_API_KEY = os.environ.get("MAILGUN_API_KEY")
+    FROM_EMAIL = os.environ.get("SENDER_EMAIL")
     FROM_TITLE = "Audiobook Builder"
-    FROM_EMAIL = ""
 
     @classmethod
     def send_email(
